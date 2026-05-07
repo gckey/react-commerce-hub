67ec1b22b94dadb6c10cb6fe83841e0306d98992
gckey <abdorehman@gmail.com>
2026-05-07 12:46:53 -0600

    feat: configure /orders route in App.jsx

--- ecommerce_project/src/App.jsx
+++ ecommerce_project/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router';
 import './App.css'
 import HomePage from './components/HomePage';
 import CheckoutPage from './components/CheckoutPage';
+import OrdersPage from './components/OrdersPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   
   );
