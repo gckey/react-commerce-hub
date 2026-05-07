c266ed85114ab19b5123f7ab8b512842cad4b1a5
gckey <abdorehman@gmail.com>
2026-05-06 20:33:19 -0600

    feat: add checkout route to App

--- ecommerce_project/src/App.jsx
+++ ecommerce_project/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router';
 import './App.css'
 import HomePage from './components/HomePage';
+import CheckoutPage from './components/CheckoutPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   
   );
