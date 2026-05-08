fdd9c5807d60acdfd922a63c1f6b8115b950bfc8
gckey <abdorehman@gmail.com>
2026-05-07 22:23:36 -0600

    refractor: extract Header into a reusable component

---
+++ ecommerce_project/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="left-section">
+        <a href="/" className="header-link">
+          <img className="logo"
+            src="images/ecommerce_logo.png" />
+        </a>
+      </div>
+
+      <div class="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" />
+
+        <button className="search-button">
+          <img className="search-icon" src="images/icons/search-icon.png" />
+        </button>
+      </div>
+
+      <div className="right-section">
+        <a className="orders-link header-link" href="/orders">
+
+          <span className="orders-text">Orders</span>
+        </a>
+
+        <a className="cart-link header-link" href="/checkout">
+          <img className="cart-icon" src="images/icons/cart-icon.png" />
+          <div className="cart-quantity">3</div>
+          <div className="cart-text">Cart</div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
