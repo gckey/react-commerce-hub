d69d49c2be826ac82adea796ad972b4afda5c4e0
gckey <abdorehman@gmail.com>
2026-05-07 22:32:31 -0600

    refractor: replace hardcoded headers with shared <Header /> component

--- ecommerce_project/src/components/OrdersPage.jsx
+++ ecommerce_project/src/components/OrdersPage.jsx
@@ -1,39 +1,11 @@
-import './header.css';
+import Header from './Header';
 import './OrdersPage.css';
 
 const OrdersPage = () => {
   return (
     <>
       <title>Orders</title>
-      <div className="header">
-        <div className="left-section">
-          <a href="/" className="header-link">
-            <img className="logo" src="images/ecommerce_logo.png" />
-          </a>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="/orders">
-
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
-
+      <Header />
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
 
--- ecommerce_project/src/components/TrackingPage.jsx
+++ ecommerce_project/src/components/TrackingPage.jsx
@@ -1,3 +1,4 @@
+import Header from './Header';
 import './header.css';
 import './TrackingPage.css';
 
@@ -5,35 +6,7 @@ const TrackingPage = () => {
   return (
     <>
       <title>Trackin Your Order</title>
-      <div className="header">
-        <div className="left-section">
-          <a href="/" className="header-link">
-            <img className="logo"
-              src="images/ecommerce_logo.png" />
-          </a>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="/orders">
-
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
+      <Header />
 
       <div className="tracking-page">
         <div className="order-tracking">
